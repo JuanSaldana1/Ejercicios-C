e23740f6d2b8ac3535bf9cc602c46fd519d59eb3
Juan Saldaña <saldana.juan.2002@gmail.com>
2022-06-17 12:32:04 +0200

    Other changes

---
+++ cmake-build-debug/CMakeFiles/Curso_C.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/juan.saldana/Documents/Curso C/ejercicio11.c" "CMakeFiles/Curso_C.dir/ejercicio11.c.obj" "gcc" "CMakeFiles/Curso_C.dir/ejercicio11.c.obj.d"
+  "C:/Users/juan.saldana/Documents/Curso C/ejercicio1Internet.c" "CMakeFiles/Curso_C.dir/ejercicio1Internet.c.obj" "gcc" "CMakeFiles/Curso_C.dir/ejercicio1Internet.c.obj.d"
+  "C:/Users/juan.saldana/Documents/Curso C/ejercicio2.c" "CMakeFiles/Curso_C.dir/ejercicio2.c.obj" "gcc" "CMakeFiles/Curso_C.dir/ejercicio2.c.obj.d"
+  "C:/Users/juan.saldana/Documents/Curso C/ejercicio3.c" "CMakeFiles/Curso_C.dir/ejercicio3.c.obj" "gcc" "CMakeFiles/Curso_C.dir/ejercicio3.c.obj.d"
+  "C:/Users/juan.saldana/Documents/Curso C/gnlExam03Edition.c" "CMakeFiles/Curso_C.dir/gnlExam03Edition.c.obj" "gcc" "CMakeFiles/Curso_C.dir/gnlExam03Edition.c.obj.d"
+  "C:/Users/juan.saldana/Documents/Curso C/main.c" "CMakeFiles/Curso_C.dir/main.c.obj" "gcc" "CMakeFiles/Curso_C.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Curso_C.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Curso_C.dir/ejercicio11.c.obj"
+  "CMakeFiles/Curso_C.dir/ejercicio11.c.obj.d"
+  "CMakeFiles/Curso_C.dir/ejercicio1Internet.c.obj"
+  "CMakeFiles/Curso_C.dir/ejercicio1Internet.c.obj.d"
+  "CMakeFiles/Curso_C.dir/ejercicio2.c.obj"
+  "CMakeFiles/Curso_C.dir/ejercicio2.c.obj.d"
+  "CMakeFiles/Curso_C.dir/ejercicio3.c.obj"
+  "CMakeFiles/Curso_C.dir/ejercicio3.c.obj.d"
+  "CMakeFiles/Curso_C.dir/gnlExam03Edition.c.obj"
+  "CMakeFiles/Curso_C.dir/gnlExam03Edition.c.obj.d"
+  "CMakeFiles/Curso_C.dir/main.c.obj"
+  "CMakeFiles/Curso_C.dir/main.c.obj.d"
+  "Curso_C.exe"
+  "Curso_C.exe.manifest"
+  "Curso_C.pdb"
+  "libCurso_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Curso_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
